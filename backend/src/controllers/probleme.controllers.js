d0351e1c4c38f2aa240c36c8aea09044ee45895c
divishtk <divisht.avion@hotmail.com>
2025-05-04 19:18:29 +0530

    feat: implement problem retrieval and deletion functionality in API

--- backend/src/controllers/probleme.controllers.js
+++ backend/src/controllers/probleme.controllers.js
@@ -41,20 +41,17 @@ export const createProblem = async (req, res) => {
         expected_output: output,
       }));
 
-    
       const submissionResults = await submitBatch(submissions);
 
       console.log("Submission Results", submissionResults);
 
       const token = submissionResults.map((result) => result.token);
 
-
       const results = await pollBatchResults(token);
-
 
       for (let i = 0; i < results.length; i++) {
         const result = results[i];
-         console.log("Result-------", results);
+        console.log("Result-------", results);
         if (result.status.id !== 3) {
           return res.status(400).json({
             message: `Test case ${
@@ -85,7 +82,9 @@ export const createProblem = async (req, res) => {
         message: "Problem created successfully",
         problem: newProblem,
       });
+
     }
+
   } catch (error) {
     console.log(error);
     return res.status(500).json({
@@ -95,12 +94,118 @@ export const createProblem = async (req, res) => {
   }
 };
 
-export const getProblemById = async (req, res) => {};
+export const getProblemById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getProblem = await db.problem.findUnique({
+      where: {
+        id,
+      },
+    });
 
-export const getAllProblems = async (req, res) => {};
+    if (!getProblem) {
+      return res.status(404).json({
+        message: `Problem not found with id ${id}`,
+      });
+    }
 
-export const updateProblemById = async (req, res) => {};
+    return res.status(200).json({
+      success: true,
+      message: "Problem fetched successfully",
+      problem: getProblem,
+    });
 
-export const deleteProblemById = async (req, res) => {};
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Problem fetching problem",
+      error: error.message,
+    });
+  }
+};
+
+export const getAllProblems = async (req, res) => {
+  try {
+    const getAllProblems = await db.problem.findMany();
+
+    if (!getAllProblems) {
+      return res.status(404).json({
+        message: "No problems found",
+      });
+    }
+    
+    return res.status(200).json({
+      success: true,
+      message: "All problems fetched successfully",
+      problems: getAllProblems,
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Problem fetching all problems",
+      error: error.message,
+    });
+  }
+};
+
+export const updateProblemById = async (req, res) => {
+  try {
+    
+    const { id } = req.params ;
+
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Problem updating problem",
+      error: error.message,
+    });
+    
+  }
+};
+
+export const deleteProblemById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // if (req.user.role !== "ADMIN") {
+    //   return res.status(403).json({
+    //     message: "You are not authorized to delete a problem",
+    //   });
+    // }
+    const problemCheck = await db.problem.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!problemCheck) {
+      return res.status(404).json({
+        message: `Problem not found with id ${id}`,
+      });
+    }
+    
+     await db.problem.delete({
+      where: {
+        id,
+      }, 
+    });
+
+    return res.status(200).json({
+      success: true,
+      message:` Problem deleted successfully with id ${id}`,
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      message: "Problem deleting problem",
+      error: error.message,
+    });
+  }
+
+};
 
 export const getAllProblemsSolvedByUser = async (req, res) => {};
