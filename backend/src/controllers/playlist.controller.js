5823ac5f464a735b0f3b648891fa81c14acfc3d8
divishtk <divisht.avion@hotmail.com>
2025-05-08 17:08:48 +0530

    feat: enhance playlist functionality with detailed fetching and user-specific constraints

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -40,8 +40,79 @@ export const addProblemToPlaylist = async (req, resp) => {};
 
 export const deletePlaylist = async (req, resp) => {};
 
-export const removeProblemFromPlaylist = async (req, resp) => {};
+export const removeProblemFromPlaylist = async (req, resp) => {
+    
+};
 
-export const getAllPlayListDetailsbyId = async (req, resp) => {};
+export const getAllPlayListDetailsbyId = async (req, resp) => {
+    const { playListid } = req.params;
 
-export const getAllListDetails = async (req, resp) => {};
+    try {
+        const playlist = await db.playlist.findMany({
+            where: {
+                id : playListid,
+                userId: req.user.id
+            },
+            include: {
+                problems : {
+                    include: {
+                        problem: true
+                    }
+                },
+            }
+        });
+        if (!playlist) {
+            return resp.status(400).json({
+                success: false,
+                message: "Playlist details not found",
+            });
+        }
+
+        return resp.status(200).json({
+            success: true,
+            message: "Playlist details fetched successfully",
+            playlist
+        });
+    } catch (error) {
+        console.error("Error fetching playlist details:", error);
+        return resp.status(500).json({
+            success: false,
+            message: "Internal server error while fetching playlist details",
+        });
+        
+    }
+};
+
+export const getAllListDetails = async (req, resp) => {
+    try {
+        const getPlayListDetails = await db.playlist.findMany({
+            where: {
+                userId: req.user.id
+            },
+            include: {
+                problems : {
+                    include: {
+                        problem: true
+                    }
+                },
+            }
+        });
+        if (!getPlayListDetails) {
+            return resp.status(400).json({
+                success: false,
+                message: "Failed to get playlist",
+            });
+        }
+        return resp.status(200).json({
+            success: true,
+            message: "Playlist fetched successfully",
+            getPlayListDetails
+        });
+    } catch (error) {
+        console.error("Error fetching playlist:", error);
+        return resp.status(500).json({
+            success: false,
+            message: "Internal server error while fetching playlist problems",
+        });
+    }
+};
